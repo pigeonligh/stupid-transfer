ebb4c51097928ad549e347f425055f91259c04a0
pigeonligh <pigeonligh@hotmail.com>
2020-06-12 22:48:53 +0800

    add event manager

---
+++ include/event.h
@@ -0,0 +1,18 @@
+#ifndef _EVENT_H_
+#define _EVENT_H_
+
+#include <stdint.h>
+
+#define MAX_EPOLL_EVENT 128
+#define EVENT_BUFF_SIZE 32
+#define EVENT_READ_TIMEOUT 1000
+
+void init_event();
+void close_event();
+
+void add_event(int32_t event_fd);
+void del_event(int32_t event_fd);
+
+void read_event(struct epoll_event* event);
+
+#endif /* _EVENT_H_ */
