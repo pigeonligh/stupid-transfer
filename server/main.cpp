ec04b73cb6e110b58a8ca6dc126d625e6006f9a7
pigeonligh <pigeonligh@hotmail.com>
2020-06-19 20:17:21 +0800

    update server/main

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,17 @@
 #include <cstdio>
+#include <signal.h>
+#include <stdlib.h>
 
 #include "server.h"
 
+void handler(int sign) {
+    server_close();
+    printf("Close server\n");
+    exit(0);
+}
+
 int main() {
-    printf("this is server.\n");
+    signal(SIGINT, handler);
 
     server_start(DEFAULT_SERVER_PORT);
 }
