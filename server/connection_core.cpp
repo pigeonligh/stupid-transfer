7f94e07c7115b212cc5f0476245745014ad4fa49
Trees-D <paladin100100@gmail.com>
2020-06-27 14:59:52 +0800

    add connection_core and test

--- server/connection_core.cpp
+++ server/connection_core.cpp
@@ -2,11 +2,76 @@
 #include "packet.h"
 
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>
 
 std::string root;
 
 void set_root(const std::string &dir) {
     root = dir;
+}
+
+bool get_path(std::string dir, std::vector<std::string> &path) {
+    if (dir[dir.size() - 1] != '/')
+        dir += std::string("/");
+    if (dir.size() == 0 || dir[0] == '/')
+        return false;
+    std::vector<std::string> _path(path);
+    int last = 0;
+    for (int i = 0; i < dir.size(); ++ i) {
+        if (dir[i] == '/') {
+            if (last >= i)
+                return false;
+            std::string _dir = dir.substr(last, i - last);
+            if (_dir == std::string(".")) {
+                last = i + 1;
+                continue ;
+            }
+            else if (_dir == std::string("..")) {
+                if (_path.size() != 0)
+                    _path.pop_back();
+                else
+                    return false;
+                last = i + 1;
+            }
+            else {
+                _path.push_back(_dir);
+                last = i + 1;
+            }
+        }
+    }
+    std::swap(path, _path);
+    return true;
+}
+
+bool remove_dir(const std::string &dir) {
+    DIR *dirp;
+    struct dirent *dp;
+    struct stat dir_stat;
+    if (access(dir.data(), 0) == -1)
+        return false;
+    if (stat(dir.data(), &dir_stat) < 0)
+        return false;
+    if (S_ISREG(dir_stat.st_mode)) {
+        remove(dir.data());
+        return true;
+    }
+    else if (S_ISDIR(dir_stat.st_mode)) {
+        dirp = opendir(dir.data());
+        while ((dp = readdir(dirp)) != NULL) {
+            if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+                continue;
+            std::string dir_name = dir + std::string("/") + std::string(dp->d_name);
+            remove_dir(dir_name);
+        }
+        closedir(dirp);
+        rmdir(dir.data());
+        return true;
+    }
+    return false;
 }
 
 connection_core::connection_core() {
@@ -23,28 +88,75 @@ connection_core::~connection_core() {
 }
 
 bool connection_core::changeDirectory(const std::string &dir) {
-    // TODO: change directory
-    return false;
+    std::vector<std::string> _path(path);
+    bool result = get_path(dir, _path);
+    if (!result)
+        return false;
+    std::string res = root;
+    for (int i = 0; i < _path.size(); ++ i)
+        res += (_path[i] + std::string("/"));
+    if (access(res.data(), 0) == -1)
+        return false;
+    std::swap(path, _path);
+    return true;
 }
 
 bool connection_core::createDirectory(const std::string &dir) {
-    // TODO: create directory
-    return false;
+    std::string res = root;
+    for (int i = 0; i < path.size(); ++ i)
+        res += (path[i] + std::string("/"));
+    std::vector<std::string> _path;
+    _path.clear();
+    bool result = get_path(dir, _path);
+    if (!result)
+        return false;
+    bool flag_exist = 1;
+    for (int i = 0; i < _path.size(); ++ i) {
+        res += (_path[i] + std::string("/"));
+        if (access(res.data(), 0) == -1) {
+            flag_exist = 0;
+            if (mkdir(res.data(), 0755) == -1)
+                return false;
+        }
+    }
+    if (flag_exist)
+        return false;
+    return true;
 }
 
 bool connection_core::removeDirectory(const std::string &dir) {
-    // TODO: remove directory
-    return false;
+    std::string _path = root;
+    for (int i = 0; i < path.size(); ++ i)
+        _path += (path[i] + std::string("/"));
+    _path += (dir + std::string("/"));
+    if (access(_path.data(), 0) == -1)
+        return false;
+    return remove_dir(_path);
 }
 
 bool connection_core::removeFile(const std::string &file) {
-    // TODO: remove file
+    std::string _path = root;
+    for (int i = 0; i < path.size(); ++i)
+        _path += (path[i] + std::string("/"));
+    _path += file;
+    struct stat dir_stat;
+    if (access(_path.data(), 0) == -1)
+        return false;
+    if (stat(_path.data(), &dir_stat) < 0)
+        return false;
+    if (S_ISREG(dir_stat.st_mode)) {
+        remove(_path.data());
+        return true;
+    }
     return false;
 }
 
 uint32_t connection_core::getCurrentDirectory(uint8_t *data) {
-    // TODO: put result into data and return length
-    return 0;
+    std::string result("/");
+    for (int i = 0; i < path.size(); ++ i)
+        result += (path[i] + std::string("/"));
+    strcpy(reinterpret_cast<char*>(data), result.data());
+    return result.length();
 }
 
 bool connection_core::setWorkingStatus(uint8_t sign, const std::string &file) {
--- server/test.cpp
+++ server/test.cpp
@@ -4,23 +4,31 @@
 
 #include <stdio.h>
 
-void server_test() {
+void server_test()
+{
     // this is test
     printf("this is test.\n");
 
-    set_root("./data/");
+    set_root("/mnt/c/Users/palad/Desktop/workspace/git/FileTransfer/bin/data/");
 
     connection_core *core = new connection_core();
 
     char buff[4096];
 
-    core->createDirectory("hello");
-    core->changeDirectory("hello");
-    core->getCurrentDirectory((uint8_t *) buff);
+    printf("%d\n", core->createDirectory("hello/hi"));
+    printf("%d\n", core->createDirectory("hello/hih"));
+    printf("%d\n", core->createDirectory("hello/hio"));
+    printf("%d\n", core->changeDirectory("hello"));
+    core->getCurrentDirectory((uint8_t *)buff);
     puts(buff);
-    core->changeDirectory("..");
-    core->getCurrentDirectory((uint8_t *) buff);
+    printf("%d\n", core->changeDirectory("./hi"));
+    core->getCurrentDirectory((uint8_t *)buff);
     puts(buff);
+    printf("%d\n", core->changeDirectory("../../"));
+    core->getCurrentDirectory((uint8_t *)buff);
+    puts(buff);
+    printf("%d\n", core->removeDirectory("hello"));
+    printf("%d\n", core->removeFile("file"));
 
     delete core;
 }
